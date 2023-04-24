186d2ff482ec78b55f148fc218cdf379a1203c8f
PoladiyVictoria <vikapoladiy@gmail.com>
2023-04-24 16:49:02 +0300

    Промежуточное сохранение: Ошибка в строке 19

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,57 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void FillArray1(string[] arr, int arrSize)
+{
+    for (int i = 0; i < arrSize; i++)
+    {
+        Console.Write("Введите строчное значение: ");
+        arr[i] = Console.ReadLine();
+    }
+}
+
+string[] CreateAndFillArray2(string[] arr1, int arrSize1)
+{
+    int sum = 0;
+    for (int i = 0; i < arrSize1; i++)
+    {
+        int length = (arr1[i]).Length;
+        if (length <= 3)
+        {
+            sum = sum + 1;
+        }
+    }
+    string[] arr2 = new string[sum];
+    int j = 0;
+    for (int i = 0; i < arrSize1; i++)
+    {
+        int length = (arr1[i]).Length;
+        if (length <= 3)
+        {
+            arr2[j] = arr1[i];
+            j++;
+        }
+    }
+    return arr2;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine("]");
+}
+
+
+Random R = new Random();
+int Size1 = R.Next(0, 10);
+string[] arrayLong = new string[Size1];
+string[] arrayShort = CreateAndFillArray2(arrayLong, Size1);
+
+FillArray1(arrayLong, Size1);
+PrintArray(arrayLong);
+PrintArray(arrayShort);
